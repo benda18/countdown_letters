c8d56611813a41f597de80052a03dee67cd3e08f
Tim Bender <bendertj@gmail.com>
2024-03-26 18:12:09 -0400

    dictionary created

--- R/explore.R
+++ R/explore.R
@@ -14,4 +14,12 @@ lv <- c("a", "e", "i", "o", "u")
 lc <- letters[!letters %in% lv]
 
 
+dict <- lexicon::grady_augmented
+mod.dict <- dict
+mod.dict <- mod.dict[!grepl(pattern = "\'", x = mod.dict)]
+mod.dict <- mod.dict[nchar(mod.dict) <= 8 & 
+                       nchar(mod.dict) > 1]
+
+
+
 
