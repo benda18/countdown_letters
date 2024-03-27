1260d88ff6cfa32d5fd14bacf8d291512157546f
Tim Bender <bendertj@gmail.com>
2024-03-27 13:33:30 -0400

    got reactive input letters working

--- shiny_countdown_letters/app.R
+++ shiny_countdown_letters/app.R
@@ -107,100 +107,101 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
  
-  # vars----
-  lv <- c("a", "e", "i", "o", "u")
-  lc <- letters[!letters %in% lv]
+  # # vars----
+  # lv <- c("a", "e", "i", "o", "u")
+  # lc <- letters[!letters %in% lv]
   
   # load list of words----
   mod.dict <- readRDS("scrabble_2to8char.rds")
   
-  # GAME STARTS----
-  # choose 5 consonants
-  c5 <- sample(lc, size = 5, replace = T)
-  
-  # choose 3 vowels
-  v3 <- sample(lv, size = 3, replace = T)
-  rm(lc,lv)
-  
-  # assemble the scrambled word
-  scramble <- c(c5,v3) |> 
-    sort() |>
-    toupper()
-  rm(c5,v3)
-  
-  
-  # make a list of permitted_letters
-  permitted_ltrs <- sort(unique(scramble))
-  
-  # make a vector of letters_not.in
-  prohibited_ltrs <- LETTERS[!LETTERS %in% permitted_ltrs]
-  
-  # find words in dictory with any prohibited_ltrs
-  prohibited_words <- grep(pattern = paste(prohibited_ltrs, sep = "|", collapse = "|"), 
-                           x = mod.dict, 
-                           ignore.case = T, 
-                           value = T)
-  
-  # all possible words
-  possible_words <- mod.dict[!mod.dict %in% prohibited_words]
-  rm(permitted_ltrs, prohibited_ltrs, prohibited_words)
-  # remove list of words
-  rm(mod.dict)
-  
-  # definite words - no repeating letters
-  nchar_unique <- strsplit(possible_words, "") |>
-    lapply(unique) |>
-    lapply(length) |>
-    unlist()
-  
-  nchar_full <- nchar(possible_words)
-  dw_words <- possible_words[nchar_unique == nchar_full]
-  print(dw_words)
-  rm(nchar_full, nchar_unique)
-  
-  # remaining possible words
-  possible_words <- possible_words[!possible_words %in% dw_words]
-  
-  
-  # remove words that have more letters than allowed
-  
-  for(i.word in possible_words){
-    temp.ltrs <- strsplit(x = i.word, 
-                          split = "") |>
-      unlist()
-    all.letters.good <- NULL
-    for(i.ltr in unique(temp.ltrs)){
-      # number of i.ltr in possible word
-      n_temp.ltr     <- sum(i.ltr == temp.ltrs)
-      # number of i.ltr in scramble word
-      n_scramble.ltr <- sum(i.ltr == tolower(unlist(strsplit(x = scramble, ""))))
-      
-      if(n_scramble.ltr >= n_temp.ltr){
-        
-        all.letters.good <- c(all.letters.good, T)
-      }else{
-        all.letters.good <- c(all.letters.good, F)
-      }
-    }
-    if(all(all.letters.good)){
-      #stop("it's good")
-      # if all letters are good, add to definite words
-      dw_words <- c(dw_words, i.word)
-    }
-    # remove from remaining possible words
-    possible_words <- possible_words[!possible_words %in% i.word]
-    
-  }
-  rm(i.word, temp.ltrs, i.ltr, n_scramble.ltr, n_temp.ltr, all.letters.good, 
-     possible_words)
-  
-  dw_words
-  
-  longest_words <- dw_words[nchar(dw_words) == max(nchar(dw_words))]
-  
-  scramble
-  
   output$fullWords <- renderText({
+    # GAME STARTS----
+    # choose 5 consonants
+    #c5 <- sample(lc, size = 5, replace = T)
+    c5 <- c(input$c1, input$c2, input$c3, 
+            input$c4, input$c5)
+    
+    # choose 3 vowels
+    #v3 <- sample(lv, size = 3, replace = T)
+    v3 <- c(input$v1, input$v2, input$v3)
+    
+    #rm(lc,lv)
+    
+    # assemble the scrambled word
+    scramble <- c(c5,v3) |> 
+      sort() |>
+      toupper()
+    rm(c5,v3)
+    
+    
+    # make a list of permitted_letters
+    permitted_ltrs <- sort(unique(scramble))
+    
+    # make a vector of letters_not.in
+    prohibited_ltrs <- LETTERS[!LETTERS %in% permitted_ltrs]
+    
+    # find words in dictory with any prohibited_ltrs
+    prohibited_words <- grep(pattern = paste(prohibited_ltrs, sep = "|", collapse = "|"), 
+                             x = mod.dict, 
+                             ignore.case = T, 
+                             value = T)
+    
+    # all possible words
+    possible_words <- mod.dict[!mod.dict %in% prohibited_words]
+    rm(permitted_ltrs, prohibited_ltrs, prohibited_words)
+    # remove list of words
+    rm(mod.dict)
+    
+    # definite words - no repeating letters
+    nchar_unique <- strsplit(possible_words, "") |>
+      lapply(unique) |>
+      lapply(length) |>
+      unlist()
+    
+    nchar_full <- nchar(possible_words)
+    dw_words <- possible_words[nchar_unique == nchar_full]
+    #print(dw_words)
+    rm(nchar_full, nchar_unique)
+    
+    # remaining possible words
+    possible_words <- possible_words[!possible_words %in% dw_words]
+    
+    
+    # remove words that have more letters than allowed
+    
+    for(i.word in possible_words){
+      temp.ltrs <- strsplit(x = i.word, 
+                            split = "") |>
+        unlist()
+      all.letters.good <- NULL
+      for(i.ltr in unique(temp.ltrs)){
+        # number of i.ltr in possible word
+        n_temp.ltr     <- sum(i.ltr == temp.ltrs)
+        # number of i.ltr in scramble word
+        n_scramble.ltr <- sum(i.ltr == tolower(unlist(strsplit(x = scramble, ""))))
+        
+        if(n_scramble.ltr >= n_temp.ltr){
+          
+          all.letters.good <- c(all.letters.good, T)
+        }else{
+          all.letters.good <- c(all.letters.good, F)
+        }
+      }
+      if(all(all.letters.good)){
+        #stop("it's good")
+        # if all letters are good, add to definite words
+        dw_words <- c(dw_words, i.word)
+      }
+      # remove from remaining possible words
+      possible_words <- possible_words[!possible_words %in% i.word]
+      
+    }
+    rm(i.word, temp.ltrs, i.ltr, n_scramble.ltr, n_temp.ltr, all.letters.good, 
+       possible_words)
+    
+    #dw_words
+    
+    longest_words <- dw_words[nchar(dw_words) == max(nchar(dw_words))]
     longest_words
     })
   
