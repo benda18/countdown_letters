5983295d894dd105d3e173f677eec3404981386d
Tim Bender <bendertj@gmail.com>
2024-03-27 11:54:09 -0400

    periodic

--- R/explore.R
+++ R/explore.R
@@ -32,4 +32,27 @@ scramble <- c(c5,v3) |>
   sort() |>
   toupper()
 
-# find dict words with scramble letters
+# make a list of permitted_letters
+permitted_ltrs <- sort(unique(scramble))
+
+# make a vector of letters_not.in
+prohibited_ltrs <- LETTERS[!LETTERS %in% permitted_ltrs]
+
+# find words in dictory with any prohibited_ltrs
+prohibited_words <- grep(pattern = paste(prohibited_ltrs, sep = "|", collapse = "|"), 
+     x = mod.dict, 
+     ignore.case = T, 
+     value = T)
+
+# all possible words
+possible_words <- mod.dict[!mod.dict %in% prohibited_words]
+
+# definite words - no repeating letters
+nchar_unique <- strsplit(possible_words, "") |>
+  lapply(unique) |>
+  lapply(length) |>
+  unlist()
+
+nchar_full <- nchar(possible_words)
+dw_no.rep.ltrs <- possible_words[nchar_unique == nchar_full]
+rm(nchar_full, nchar_unique)
